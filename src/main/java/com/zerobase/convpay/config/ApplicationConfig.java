1db5d60f81da0857afc1f9d4893ee4335d8df984
G1 <lovyellow923@naver.com>
2024-10-28 03:18:43 +0900

    ApplicationConfig -> 스프링의 Application Context 으로 변경

--- src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,27 +1,38 @@
 package com.zerobase.convpay.config;
 
 import com.zerobase.convpay.service.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
 // 전체 애플리케이션의 설정을 결정함.
+@Configuration
 public class ApplicationConfig {
-    public ConveniencePayService conveniencePayServiceDiscountConvenience(){
+
+    @Bean
+    public ConveniencePayService conveniencePayService(){
         return new ConveniencePayService(
                 new HashSet<>(
-                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
+                        Arrays.asList(moneyAdapter(), cardAdapter())
                 ),
-                new DiscountByConvenience()
+                getDiscountByConvenience()
         );
     }
 
-    public ConveniencePayService conveniencePayServiceDiscountPayMethod(){
-        return new ConveniencePayService(
-                new HashSet<>(
-                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
-                ),
-                new DiscountPayMethod()
-        );
+    @Bean
+    public static CardAdapter cardAdapter() {
+        return new CardAdapter();
+    }
+
+    @Bean
+    public static MoneyAdapter moneyAdapter() {
+        return new MoneyAdapter();
+    }
+
+    @Bean
+    public static DiscountByConvenience getDiscountByConvenience() {
+        return new DiscountByConvenience();
     }
 }
