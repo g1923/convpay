c58d1063ee429e31f24381e32858c5e104535a51
G1 <lovyellow923@naver.com>
2024-10-31 22:50:08 +0900

    xml -> Javaconfig로 변경(@ComponentScan basePackageClasses 등록)
    * xml의 오타로 인한 빈번하게 일어나는 문제때문에 spring4부터는 JavaConfig를 사용하기 많이 사용하기 시작함.
    
    * Spring에서 DI를 다양한 방법 4가지 을 배워봄.
        1) xml 로 일일이 bean을 등록함
        2) xml componentscan을 이용하여 bean을 등록함
        3) javaConfig로 일일이 bean을 등록하는 방법
        4) javaConfig로 componentscan을 하는 방법
    
    * SpringBoot에서는 ApplicationConfig를 설정하지 않아도됨.
    * 프로젝트Application.java 에 자동설정되어있음

--- src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,38 +1,37 @@
-//package com.zerobase.convpay.config;
-//
-//import com.zerobase.convpay.service.*;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.Arrays;
-//import java.util.HashSet;
-//
-//// 전체 애플리케이션의 설정을 결정함.
-//@Configuration
-//public class ApplicationConfig {
-//
-//    @Bean
-//    public ConveniencePayService conveniencePayService(){
-//        return new ConveniencePayService(
-//                new HashSet<>(
-//                        Arrays.asList(moneyAdapter(), cardAdapter())
-//                ),
-//                discountByConvenience()
-//        );
-//    }
-//
-//    @Bean
-//    public static CardAdapter cardAdapter() {
-//        return new CardAdapter();
-//    }
-//
-//    @Bean
-//    public static MoneyAdapter moneyAdapter() {
-//        return new MoneyAdapter();
-//    }
-//
-//    @Bean
-//    public static DiscountByConvenience discountByConvenience() {
-//        return new DiscountByConvenience();
-//    }
-//}
+package com.zerobase.convpay.config;
+
+import com.zerobase.convpay.ConvpayApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// 전체 애플리케이션의 설정을 결정함.
+@Configuration
+//@ComponentScan(basePackages = "com.zerobase.convpay")
+@ComponentScan(basePackageClasses = ConvpayApplication.class) // ConvpayApplication 클래스위치부터 하위 클래스의 모든 파일들을 스캔함을 의미
+public class ApplicationConfig {
+
+
+}
+
+/*
+DI를 다양한 방법 4가지 을 배워봄.
+    1) xml 로 일일이 bean을 등록함
+    2) xml componentscan을 이용하여 bean을 등록함
+    3) javaConfig로 일일이 bean을 등록하는 방법
+    4) javaConfig로 componentscan을 하는 방법
+*/
+
+
+/*
+* SpringBoot에서는 ApplicationConfig를 설정하지 않아도됨.
+* ConvpayApplication.java에서 이미 설정되어서 파일이 생성됨
+
+@SpringBootApplication
+public class ConvpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvpayApplication.class, args);
+	}
+
+}
+* */
