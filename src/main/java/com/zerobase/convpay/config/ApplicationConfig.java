3dd8ff87bb62eec053764a968ac256507951f8a5
G1 <lovyellow923@naver.com>
2024-11-01 21:18:59 +0900

    chapter05. 스프링부가기능 필기

--- src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,37 +1,24 @@
 package com.zerobase.convpay.config;
 
 import com.zerobase.convpay.ConvpayApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
 
 // 전체 애플리케이션의 설정을 결정함.
 @Configuration
-//@ComponentScan(basePackages = "com.zerobase.convpay")
 @ComponentScan(basePackageClasses = ConvpayApplication.class) // ConvpayApplication 클래스위치부터 하위 클래스의 모든 파일들을 스캔함을 의미
 public class ApplicationConfig {
-
-
+//    @Autowired
+//    private ApplicationContext applicationContext;
+//
+//    public void getResource() throws IOException {
+//        Resource resource = applicationContext.getResource("myTeplate.txt");
+//
+//        System.out.println(resource.contentLength() + "");
+//    }
 }
-
-/*
-DI를 다양한 방법 4가지 을 배워봄.
-    1) xml 로 일일이 bean을 등록함
-    2) xml componentscan을 이용하여 bean을 등록함
-    3) javaConfig로 일일이 bean을 등록하는 방법
-    4) javaConfig로 componentscan을 하는 방법
-*/
-
-
-/*
-* SpringBoot에서는 ApplicationConfig를 설정하지 않아도됨.
-* ConvpayApplication.java에서 이미 설정되어서 파일이 생성됨
-
-@SpringBootApplication
-public class ConvpayApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ConvpayApplication.class, args);
-	}
-
-}
-* */
